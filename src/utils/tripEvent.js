da8b81a8f4fd318de40a3cbf198b079d548c7aaa
SapGri <sapgri360@gmail.com>
2024-05-20 19:17:34 +0300

    Шаблонизируй то

--- src/utils/tripEvent.js
+++ src/utils/tripEvent.js
@@ -1,5 +1,7 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import { FilterType } from '../const.js';
+
 dayjs.extend(duration);
 
 const getDuration = (dateFrom, dateTo) => {
@@ -26,4 +28,16 @@ const getDuration = (dateFrom, dateTo) => {
 
 const humanizeEventDueDate = (dueDate, format) => dueDate ? dayjs(dueDate).format(format) : '';
 
-export { humanizeEventDueDate, getDuration };
+const isFutureEvents = ((events) => events.filter((event) => new Date(event.dateFrom) > new Date()));
+const isPresentEvents = ((events) => events.filter((event) => new Date(event.dateFrom) <= new Date()
+  && new Date(event.dateTo) >= new Date()));
+const isPastEvents = ((events) => events.filter((event) => new Date(event.dateTo) < new Date()));
+
+const filter = {
+  [FilterType.EVERYTHING]: (events) => events,
+  [FilterType.FUTURE]: (events) => isFutureEvents(events),
+  [FilterType.PRESENT]: (events) => isPresentEvents(events),
+  [FilterType.PAST]: (events) => isPastEvents(events),
+};
+
+export { humanizeEventDueDate, getDuration, filter };
--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -1,16 +1,23 @@
 import AbstractView from '../framework/view/abstract-view.js';
 import { SORT_TYPES } from '../const.js';
 
-const createSortingItemTemplate = (type) => `
-  <div class="trip-sort__item  trip-sort__item--${type}">
-    <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio"
-      name="trip-sort" value="sort-${type}"${type}"${type === SORT_TYPES[0] ? ' checked' : ''}>
+const createSortingItemTemplate = (type, isChecked) => `
+  <div
+    class="trip-sort__item
+    trip-sort__item--${type}">
+      <input
+        id="sort-${type}"
+        class="trip-sort__input  visually-hidden"
+        type="radio"
+        name="trip-sort"
+        value="sort-${type}"${type}"
+        ${isChecked ? ' checked' : ''}>
     <label class="trip-sort__btn" for="sort-${type}">${type}</label>
   </div>`;
 
 const createSortingTemplate = () => `
   <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    ${SORT_TYPES.map((type) => createSortingItemTemplate(type)).join('')}
+    ${SORT_TYPES.map((type, index) => createSortingItemTemplate(type, index === 0)).join('')}
   </form>`;
 
 export default class SortingView extends AbstractView {
