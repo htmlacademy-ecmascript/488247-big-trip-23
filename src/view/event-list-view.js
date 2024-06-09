7a067ba53f268507393a593e7e6f09cf1304149e
SapGri <sapgri360@gmail.com>
2024-06-09 11:47:03 +0300

    fix: Удалил ненужные файлы

--- src/view/event-list-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../framework/view/abstract-view.js';
-
-const createListTemplate = () => '<ul class="trip-events__list"></ul>';
-
-export default class EventListView extends AbstractView {
-  get template() {
-    return createListTemplate();
-  }
-}
